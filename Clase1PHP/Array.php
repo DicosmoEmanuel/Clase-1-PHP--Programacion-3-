890f26b25674015c3a168bd94dd82d6c92bf3523
Dicosmo Emanuel Alejandro <emanuel.dicosmo@gmail.com>
2015-08-15 15:34:17 -0300

    Finalizacion del ejercicio con Arrays
    Se agrego un switch dentro del foreach y agregando la etiqueta <font> se
    le dio el color al texto.

--- Clase1PHP/Array.php
+++ Clase1PHP/Array.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+
 <html>
 	<head>
 		<title>Ejercicio de Array</title>
@@ -8,20 +8,28 @@
 
 				$colores = array("Rojo","Azul","Verde","Amarillo","Violeta");
 
-				
+				 
 
 				foreach ($colores as $valor)
-				{
-					switch ($valor) {
-						case 'Rojo':
-							 
-							break;
-						
-						default:
-							# code...
-							break;
-					}
-				   
+				{					
+				   switch ($valor)
+				   {
+				   		case 'Rojo':
+				   			echo "<font color='red'>$valor</font><br>";
+				   			break;
+				   		case 'Azul':
+				   			echo "<font color='blue'>$valor</font><br>";
+				   			break;
+				   		case 'Verde':
+				   			echo "<font color='green'>$valor</font><br>";
+				   			break;
+				   		case 'Amarillo':
+				   			echo "<font color='yellow'>$valor</font><br>";
+				   			break;
+				   		case 'Violeta':
+				   			echo "<font color='violet'>$valor</font><br>";
+				   			break;
+				  }	
 				}
 			?>
      	</body>
